94daee741e10bd7df30665f935d798064022b192
sfefgffsgi <e-washida@gikentrastem.com>
2024-07-04 10:07:44 +0900

    「ESLint, Prettierを導入しよう」「SASSを導入しよう」

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -11,7 +11,7 @@ import { SignUp } from "../pages/SignUp";
 import { EditList } from "../pages/EditList";
 
 export const Router = () => {
-  const auth = useSelector((state) => state.auth.isSignIn)
+  const auth = useSelector((state) => state.auth.isSignIn);
 
   return (
     <BrowserRouter>
@@ -21,20 +21,17 @@ export const Router = () => {
         {auth ? (
           <>
             <Route path="/" element={<Home />} />
-            <Route path="/task/new" element={<NewTask/>} />
-            <Route path="/list/new" element={<NewList/>} />
-            <Route path="/lists/:listId/tasks/:taskId" element={<EditTask/>} />
-            <Route path="/lists/:listId/edit" element={<EditList/>} />
+            <Route path="/task/new" element={<NewTask />} />
+            <Route path="/list/new" element={<NewList />} />
+            <Route path="/lists/:listId/tasks/:taskId" element={<EditTask />} />
+            <Route path="/lists/:listId/edit" element={<EditList />} />
           </>
         ) : (
-          <Route
-            path="/"
-            element={<Navigate replace to="/signin" />}
-          />
+          <Route path="/" element={<Navigate replace to="/signin" />} />
           // <Navigate replace to="/signin" />
         )}
         <Route element={NotFound} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
