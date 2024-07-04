25e278cf5133d6de271d7a2c1355f76691ac12ec
sfefgffsgi <e-washida@gikentrastem.com>
2024-07-04 11:13:46 +0900

    「ライブラリのバージョンアップをしよう」修正漏れ

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -9,7 +9,7 @@ import { url } from "../const";
 import "./signUp.scss";
 
 export const SignUp = () => {
-  const history = useNavigate();
+  const navigate = useNavigate();
   const auth = useSelector((state) => state.auth.isSignIn);
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
@@ -26,14 +26,13 @@ export const SignUp = () => {
       name: name,
       password: password,
     };
-
     axios
       .post(`${url}/users`, data)
       .then((res) => {
         const token = res.data.token;
         dispatch(signIn());
         setCookie("token", token);
-        history.push("/");
+        navigate("/");
       })
       .catch((err) => {
         setErrorMessge(`サインアップに失敗しました。 ${err}`);
